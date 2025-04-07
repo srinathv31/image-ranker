5ad2810a6e339d317463d3fc5fc671b1c54dcaad
Srinath Venkatesh <srinathvb1@gmail.com>
2025-04-07 01:17:12 -0400

    github actions build fix

--- forge.config.ts
+++ forge.config.ts
@@ -14,7 +14,7 @@ import { rendererConfig } from "./webpack.renderer.config";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    extraResource: [`./dist/api${process.platform === "win32" ? ".exe" : ""}`],
+    extraResource: ["./dist/api"],
     icon: "./src/assets/icon",
   },
   rebuildConfig: {},
